b443bd255781a03f9f39ea7e4c544da90baff690
raulpy271 <raulrufino0@gmail.com>
2025-01-10 23:03:15 -0300

    Add logger APIRouter

--- api/api/app.py
+++ api/api/app.py
@@ -1,13 +1,21 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from api import public
+from api import user
 from api import auth
+
 
 def create_app():
     app = FastAPI()
+    logged_router = APIRouter(
+        prefix="/logged",
+        tags=["logged"]
+    )
+    logged_router.include_router(user.router)
     app.include_router(public.router)
     app.include_router(auth.router)
+    app.include_router(logged_router)
     app.add_middleware(auth.CheckAuthMiddleware)
     return app
 
--- api/api/public.py
+++ api/api/public.py
@@ -1,18 +1,10 @@
 
-from http import HTTPStatus
-from typing import Annotated
+from fastapi import APIRouter
 
-from fastapi import APIRouter, Depends
-
-from sqlalchemy import (
-    text,
-    select)
+from sqlalchemy import text
 
 from api import redis
-from api.models import User
 from api.models import Session
-from api import dto
-from api.auth import create_hash_salt, get_user_id
 
 router = APIRouter(
     prefix="/public",
@@ -30,25 +22,6 @@ def test_redis():
     client = redis.get_default_client()
     client.set('test', 'hello')
     return client.get('test')
-
-@router.post("/logged/create", status_code=HTTPStatus.CREATED)
-def create(data: dto.CreateUserIn):
-    with Session() as session:
-        data = data.model_dump()
-        password = data.pop('password')
-        phash, salt = create_hash_salt(password)
-        data['password_hash'] = phash
-        data['password_salt'] = salt
-        user = User(**data)
-        session.add(user)
-        session.commit()
-    return {}
-
-@router.get("/logged/me")
-def me(user_id: Annotated[str, Depends(get_user_id)]):
-    with Session() as session:
-        u = session.scalars(select(User).where(User.id == user_id)).one()
-    return u.to_dict()
 
 
 @router.get("/")
