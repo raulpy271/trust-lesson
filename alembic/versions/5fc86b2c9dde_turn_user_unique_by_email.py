1f436bbf83aeabd1cfac5804eda3930b556e6886
raulpy271 <raulrufino0@gmail.com>
2025-01-12 20:11:05 -0300

    Add unique constraint in user email

---
+++ alembic/versions/5fc86b2c9dde_turn_user_unique_by_email.py
@@ -0,0 +1,27 @@
+"""Turn user unique by email
+
+Revision ID: 5fc86b2c9dde
+Revises: bc81aa7eabb3
+Create Date: 2025-01-12 19:41:19.189558
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5fc86b2c9dde'
+down_revision: Union[str, None] = 'bc81aa7eabb3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint("user_email_unique", "user", ["email"])
+
+
+def downgrade() -> None:
+    op.drop_constraint("user_email_unique", "user")
+
