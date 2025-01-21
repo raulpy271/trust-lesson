b19f42e01bf4c3682aaa07bcd3511d02f8b89ee7
raulpy271 <raulrufino0@gmail.com>
2025-01-20 23:06:44 -0300

    Create initial structure to deploy on azure

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0.1"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "trustLessonResourceGroup"
+  location = var.region
+}
---
+++ infra/variables.tf
@@ -0,0 +1,4 @@
+
+variable "region" {
+  default = "westus2"
+}
