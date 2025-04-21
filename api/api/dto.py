c14bd95a67f1f936163a08340cea60e9552c51b5
raulpy271 <raulrufino0@gmail.com>
2025-04-21 19:52:11 -0300

    Add health check route

--- api/api/dto.py
+++ api/api/dto.py
@@ -19,3 +19,12 @@ class LoginIn(BaseModel):
 class ValidationIn(BaseModel):
     lesson_id: UUID
     file: UploadFile
+
+
+class HealthOut(BaseModel):
+    database_healthy: bool = True
+    database_error: str | None = None
+    redis_healthy: bool = True
+    redis_error: str | None = None
+    storage_healthy: bool = True
+    storage_error: str | None = None
--- api/api/public.py
+++ api/api/public.py
@@ -1,11 +1,15 @@
+from http import HTTPStatus
 from uuid import uuid4
 
-from fastapi import APIRouter
-from sqlalchemy import text
+from fastapi import APIRouter, Response
+from sqlalchemy import select, text
+from sqlalchemy import exc
+from redis.exceptions import RedisError
 
 from api import redis
-from api.models import Session
+from api.models import Session, User
 from api.azure import get_container_image
+from api.dto import HealthOut
 
 router = APIRouter(prefix="/public", tags=["public"])
 
@@ -31,6 +35,30 @@ async def test_storage():
     await container.upload_blob(filename, "Hello World from storage", encoding="utf-8")
     stream = await container.download_blob(filename, encoding="utf-8")
     return await stream.readall()
+
+
+@router.get("/health", response_model=HealthOut)
+async def health(response: Response):
+    error = False
+    result = HealthOut()
+    try:
+        with Session() as session:
+            session.scalars(select(User)).one_or_none()
+    except (exc.DisconnectionError, exc.OperationalError) as e:
+        result.database_healthy = False
+        result.database_error = str(e)
+        error = True
+    try:
+        client = redis.get_default_client()
+        client.info()
+    except RedisError as e:
+        result.redis_healthy = False
+        result.redis_error = str(e)
+        error = True
+    response.status_code = (
+        HTTPStatus.OK if not error else HTTPStatus.INTERNAL_SERVER_ERROR
+    )
+    return result
 
 
 @router.get("/")
