ec642eaaa7d10562632dfdc6c0020a972f1f1fac
Raul <raulrufino0@gmail.com>
2025-10-12 14:58:57 -0300

    Add function to create validation identity

--- functions/functions/validator.py
+++ functions/functions/validator.py
@@ -11,12 +11,18 @@ from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.ai.documentintelligence.models import (
     AnalyzeDocumentRequest,
     AnalyzeResult,
+    DocumentAnalysisFeature,
 )
 
 from api.settings import STORAGE_URL
-from api.models import LessonValidation, IdentityValidation
 from api.azure.storage import generate_sas
 from api.jobs.validate_images import Validator
+from api.models import (
+    AsyncSession,
+    LessonValidation,
+    IdentityValidation,
+    IdentityType,
+)
 from functions.settings import (
     VISION_ENDPOINT,
     VISION_APIKEY,
@@ -81,6 +87,8 @@ async def validate_identity(user_id: UUID, filename: str) -> AnalyzeResult:
                 "prebuilt-idDocument",
                 request,
                 locale=DOCUMENT_INTELLIGENCE_LOCALE,
+                query_fields=["Fullname", "MotherFullname", "FatherFullname"],
+                features=[DocumentAnalysisFeature.QUERY_FIELDS],
             )
             result = await poller.result()
             if result.warnings:
@@ -97,4 +105,60 @@ async def validate_identity(user_id: UUID, filename: str) -> AnalyzeResult:
 async def create_validation_identity(
     validation_res: AnalyzeResult, user_id: UUID, filename: str
 ) -> IdentityValidation:
-    pass
+    fields_mapping = {
+        "Fullname": "fullname",
+        "DocumentNumber": "identity_code",
+        "DateOfBirth": "birth_date",
+        "DateOfExpiration": "expiration_date",
+        "DateOfIssue": "issued_date",
+        "IssuingAuthority": "issuing_authority",
+        "Region": "country_state",
+    }
+    doctypes = {
+        "idDocument.nationalIdentityCard": IdentityType.IDENTITY_CARD,
+        "idDocument.driverLicense": IdentityType.DRIVER_LICENSE,
+        "idDocument.passport": IdentityType.PASSPORT,
+    }
+    validation_args = {}
+    for document in validation_res.documents:
+        logging.info(document.doc_type)
+        logging.info(str(document.fields))
+        validation_args["type"] = doctypes.get(document.doc_type, None)
+        validation_args["type_confidence"] = document.confidence
+        for field_key, field in document.fields.items():
+            if field_key in fields_mapping and field.get("content"):
+                if field["type"] == "string":
+                    validation_args[fields_mapping[field_key]] = field["valueString"]
+                elif field["type"] == "date":
+                    validation_args[fields_mapping[field_key]] = field["valueDate"]
+                validation_args[fields_mapping[field_key] + "_confidence"] = field[
+                    "confidence"
+                ]
+        if "MotherFullname" in document.fields and document.fields[
+            "MotherFullname"
+        ].get("valueString"):
+            validation_args["parent_fullname"] = document.fields["MotherFullname"][
+                "valueString"
+            ]
+            validation_args["parent_fullname_confidence"] = document.fields[
+                "MotherFullname"
+            ]["confidence"]
+        elif "FatherFullname" in document.fields and document.fields[
+            "FatherFullname"
+        ].get("valueString"):
+            validation_args["parent_fullname"] = document.fields["FatherFullname"][
+                "valueString"
+            ]
+            validation_args["parent_fullname_confidence"] = document.fields[
+                "FatherFullname"
+            ]["confidence"]
+    logging.info(str(validation_args))
+    async with AsyncSession() as session:
+        validation_args["user_id"] = user_id
+        validation_args["image_path"] = filename
+        validation_args["validated"] = True
+        validation_args["validated_success"] = True
+        validation = IdentityValidation(**validation_args)
+        session.add(validation)
+        await session.commit()
+    return validation
